7567c3acff85ec4f678be3fcc361f7224822a64d
AkuthotaSateesh <Sateesh.Kumar@wisteli.com>
2021-12-12 22:57:22 +0530

    origin cross errors solved

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app,allow_headers='*',)
+app.config['CORS_HEADERS']='content_type'
 data_table = table()
 
 
@@ -18,19 +19,19 @@ def insert_data():
             d = request.get_json()
             msg = data_table.insert_data(d)
             results = {'message': msg}
-            # response = jsonify(results)
-            # response.headers.add('Access-Control-Allow-Origin', '*')
-            return results
+            response = jsonify(results)
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
         except Exception as e:
             results = {"error": str(e)}
-            # response = jsonify(results)
-            # response.headers.add('Access-Control-Allow-Origin', '*')
-            return results
+            response = jsonify(results)
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
 
 
 @app.route('/getdata', defaults={'page': 1}, methods=['POST', 'GET'])
 @app.route('/getdata/page/<int:page>', methods=['POST', "GET"])
-@cross_origin()
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_data_from_db(page):
     page = request.args.get('page')
     room_no = request.args.get('room')
@@ -39,31 +40,39 @@ def get_data_from_db(page):
         if page is None:
             page = int(1)
         alldata = data_table.fetch_data_from_db(page)
-        # response = jsonify(alldata)
-        # response.headers.add('Access-Control-Allow-Origin', '*')
-        return alldata
+        response = jsonify(alldata)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers["Content-Type"]='application/json'
+        return response
 
 
 @app.route('/total_data', methods=['GET'])
-@cross_origin()
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_total_data_db():
+    print('content type',request.content_type)
+    request.content_type='application/json'
     if request.method == 'GET':
         data = data_table.get_total_data()
-        # response = jsonify(data)
-        # response.headers.add('Access-Control-Allow-Origin', '*')
-        return data
+        response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        return response
 
 
-@app.route('/filter_data', methods=['GET'])
-@cross_origin()
+@app.route('/filter_data', methods=['GET','POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def filter_data():
+
+    print('content type',dict(request.headers))
+
     if request.method == 'GET':
         filter_value = request.args.get('filter_column')
         value = request.args.get('value')
         data = data_table.filter_data(filter_value, value)
-        # response = jsonify(data)
-        # response.headers.add('Access-Control-Allow-Origin', '*')
-        return data
+        response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
 
 if __name__ == '__main__':
